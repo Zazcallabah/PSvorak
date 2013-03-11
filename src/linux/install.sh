f351da423a710fe3c72d66ba5f3b2db411da3fc1
Peter Hamberg <zazcallabah@gmail.com>
2013-03-11 19:04:07 +0100

    fix install

--- src/linux/install.sh
+++ src/linux/install.sh
@@ -1,5 +1,4 @@
 wget https://raw.github.com/Zazcallabah/PSvorak/master/src/linux/psvorak.x11
 cat psvorak.x11 >> /usr/share/X11/xkb/symbols/se
-cat  |sed 's/<name>svdvorak</name>/<name>psvorak</name>/g' < /usr/share/X11/xkb/rules/evdev.xml > tmp.txt
-
-#<variant><configItem><name>psvorak</name><description>PSvorak</description></configItem></variant>
+#simple hack variant, just replace svdvorak
+sed -i 's/<name>svdvorak<\/name>/<name>psvorak<\/name>/g' /usr/share/X11/xkb/rules/evdev.xml
