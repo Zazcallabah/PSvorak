30b01a7b3c7d425379095d5b384def6e9a150667
Peter Hamberg <github@prefect.se>
2013-03-13 10:23:27 +0100

    Update install.sh

--- src/linux/install.sh
+++ src/linux/install.sh
@@ -2,3 +2,4 @@ wget https://raw.github.com/Zazcallabah/PSvorak/master/src/linux/psvorak.x11
 cat psvorak.x11 >> /usr/share/X11/xkb/symbols/se
 #simple hack variant, just replace svdvorak
 sed -i 's/<name>svdvorak<\/name>/<name>psvorak<\/name>/g' /usr/share/X11/xkb/rules/evdev.xml
+sudo setxkbmap psvorak
