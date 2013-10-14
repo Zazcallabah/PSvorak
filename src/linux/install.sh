210b24ff67acbf3ce5cf92104e2e86b54fa1783f
Peter Hamberg <zazcallabah@gmail.com>
2013-10-14 11:48:11 +0200

    add manual instructions for raspberry pi

--- src/linux/install.sh
+++ src/linux/install.sh
@@ -3,3 +3,6 @@ cat psvorak.x11 >> /usr/share/X11/xkb/symbols/se
 #simple hack variant, just replace svdvorak
 sed -i 's/<name>svdvorak<\/name>/<name>psvorak<\/name>/g' /usr/share/X11/xkb/rules/evdev.xml
 setxkbmap psvorak
+
+# on raspberry pi you need to instead of setxkbmap edit /etc/default/keyboard
+# set XKBLAYOUT="se" and XKBVARIANT="psvorak"
